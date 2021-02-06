91a7b393ec50656d85f7db14616f066ea26073ed
Saanviarora <75076882+Saanviarora@users.noreply.github.com>
2021-02-07 00:35:07 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ var divisions=[];
 var divisionHeight=300;
 var score=0;
 function setup() {
-  createCanvas(800,800);
+  createCanvas(820,800);
 
  engine = Engine.create();
  world= engine.world;
